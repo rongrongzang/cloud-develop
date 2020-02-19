7062d7703ef08aba128ec097004580e16487dd53
zhangrong <970237820@qq.com>
2020-02-19 20:22:09 +0800

    调剂页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,62 +4,131 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    list: [{
+        title: "2020年长春工业大学化工工程学院调剂信息", //名称
+        province: "吉林省", //学校省份
+        category: "理学", //专业门类
+        profession: "高分子化学与物理", //专业
+        direction: "暂无", //研究方向
+        time: "2020-02-17", //时间
+      },
+      {
+        title: "2020年长春工业大学化工工程学院调剂信息", //名称
+        province: "吉林省", //学校省份
+        category: "理学", //专业门类
+        profession: "高分子化学与物理", //专业
+        direction: "暂无", //研究方向
+        time: "2020-02-17", //时间
+      },
+      {
+        title: "2020年长春工业大学化工工程学院调剂信息", //名称
+        province: "吉林省", //学校省份
+        category: "理学", //专业门类
+        profession: "高分子化学与物理", //专业
+        direction: "暂无", //研究方向
+        time: "2020-02-17", //时间
+      },
+      {
+        title: "2020年长春工业大学化工工程学院调剂信息", //名称
+        province: "吉林省", //学校省份
+        category: "理学", //专业门类
+        profession: "高分子化学与物理", //专业
+        direction: "暂无", //研究方向
+        time: "2020-02-17", //时间
+      }
+    ],
+    window_width: 0,
+    window_height: 0,
+    cion_width: 0,
+    cion_height: 0,
+    select_year: '0',
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  changTab: function (e) {
+    this.setData({
+      select_year: e.currentTarget.dataset.index,
     })
   },
-  getid: function () {
-    wx.cloud.callFunction({
-      name:"getopenid",
-      success(res){
-        console.log('获取openId成功', res.result.openid) 
+  //跳转 “筛选” 页面
+  changfilter: function (e) {
+    wx.navigateTo({
+      url: '../indexPages/filter/filter?id=1',
+      events: {
+        // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
+        acceptDataFromOpenedPage: function (data) {
+          console.log(data)
+        },
+        someEvent: function (data) {
+          console.log(data)
+        }
       },
-      fail(res) {
-        console.log('获取openId失败',res)
-      },
+      success: function (res) {
+        // 通过eventChannel向被打开页面传送数据
+        res.eventChannel.emit('acceptDataFromOpenerPage', {
+          data: 'test'
+        })
+      }
+    })
+  },
+  //跳转 “查看详情” 页面
+  toDetail: function (e) {
+    wx.navigateTo({
+      url: '../indexPages/detail/detail?id=1',
+    })
+  },
+  //跳转 “发布调剂信息” 页面
+  addItem: function (e) {
+    wx.navigateTo({
+      url: '../indexPages/additem/additem',
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    let that = this;
+    // 获取系统信息
+    wx.getSystemInfo({
+      success: function (res) {
+        let windowWidth = res.windowWidth;
+        let windowHeight = res.windowHeight;
+        that.setData({
+          window_width: windowWidth,
+          window_height: windowHeight,
+          cion_width: windowWidth - 10,
+          cion_height: windowHeight - 88,
+        });
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    });
+    //   if (app.globalData.userInfo) {
+    //     this.setData({
+    //       userInfo: app.globalData.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   } else if (this.data.canIUse) {
+    //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //     // 所以此处加入 callback 以防止这种情况
+    //     app.userInfoReadyCallback = res => {
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   } else {
+    //     // 在没有 open-type=getUserInfo 版本的兼容处理
+    //     wx.getUserInfo({
+    //       success: res => {
+    //         app.globalData.userInfo = res.userInfo
+    //         this.setData({
+    //           userInfo: res.userInfo,
+    //           hasUserInfo: true
+    //         })
+    //       }
+    //     })
+    //   }
+    // },
+    // getUserInfo: function (e) {
+    //   console.log(e)
+    //   app.globalData.userInfo = e.detail.userInfo
+    //   this.setData({
+    //     userInfo: e.detail.userInfo,
+    //     hasUserInfo: true
+    //   })
   }
 })
